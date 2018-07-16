3946e9ce1cb164b4ed10bd903fcf0df02e4313fb
Valdir Torres <valdyrtorres@yahoo.com.br>
2018-07-15 23:18:35 -0300

    subindo o projeto

---
+++ app/src/main/java/br/com/roentgen/dossietenis/ApplicationData.java
@@ -0,0 +1,7 @@
+package br.com.roentgen.dossietenis;
+
+public class ApplicationData  {
+    public static final String CLIENT_ID = "9bbf02a6026e4168988adffe41ede501";
+    public static final String CLIENT_SECRET = "973616031261408ab3cd5a3d06bdc3c0";
+    public static final String CALLBACK_URL = "https://github.com/valdyrtorres/androidinstagraminteg";
+}
